55eb5d36f9f59936d39a10b1f56a5d47bfc4eab9
sgracki <sgnanex@gmail.com>
2018-01-10 00:31:04 +0100

    api admin and api post working

---
+++ backend/routes/api_posts.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+const Post = require('../models/Post');
+
+router.get('/', (req, res) => {
+    Post.find({}).populate('author').exec((err, posts) => {
+        (err || !posts) ? res.sendStatus(403) : res.send(posts).status(200);
+    })
+});
+
+module.exports = router;
